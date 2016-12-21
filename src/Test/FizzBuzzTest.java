827a7b4f18f80ab80062d960b4276d8bcd8adc28
nicolalandro <nicolaxx94@live.it>
2016-12-21 14:46:37 +0100

    Complete FizzBuzzTest

--- src/Test/FizzBuzzTest.java
+++ src/Test/FizzBuzzTest.java
@@ -22,11 +22,23 @@ public class FizzBuzzTest extends TestCase {
         assertEquals("1", fizzBuzz.getValue(1));
     }
 
+    public void test_expected_fizz_FromInput9()throws Exception{
+        assertEquals("fizz", fizzBuzz.getValue(9));
+    }
+
+    public void test_expected_buzz_FromInput25()throws Exception{
+        assertEquals("buzz", fizzBuzz.getValue(25));
+    }
+
+    public void test_expected_bang_FromInput49()throws Exception{
+        assertEquals("bang", fizzBuzz.getValue(49));
+    }
+
     public void test_expected_fizz_FromInput3()throws Exception{
         assertEquals("fizz", fizzBuzz.getValue(3));
     }
 
-    public void test_expected_fizz_FromInput5()throws Exception{
+    public void test_expected_buzz_FromInput5()throws Exception{
         assertEquals("buzz", fizzBuzz.getValue(5));
     }
 
