e2073ae95b0f056e49481358732be575ae95552f
nicolalandro <nicolaxx94@live.it>
2016-12-21 13:03:19 +0100

    Refactor on FizzBuzz

--- src/NoUtil/FizzBuzz.java
+++ src/NoUtil/FizzBuzz.java
@@ -1,24 +1,30 @@
 package NoUtil;
 
 
+import java.util.ArrayList;
+
 public class FizzBuzz {
+    private  ArrayList<SostitutionRuleInterface> listOfRule;
+    public FizzBuzz(){
+        this.listOfRule = new ArrayList();
+
+        this.listOfRule.add(new SostitutionRuleNumber3());
+        this.listOfRule.add(new SostitutionRuleNumber5());
+        this.listOfRule.add(new SostitutionRuleNumber7());
+    }
 
     public String getValue(int number) {
-        SostitutionRuleNumber3 number3 = new SostitutionRuleNumber3();
-        SostitutionRuleNumber5 number5 = new SostitutionRuleNumber5();
-        SostitutionRuleNumber7 number7 = new SostitutionRuleNumber7();
+
 
         //TODO if(number==0) è un problema comune a tutti i numeri quindi per non duplicare il controllo converrebe farlo qui,
         // però logicamente dovrebbe stare nel metodo divide
 
-        if(number3.divide(number)){
-            return number3.getValue();
-        }
-        else if(number5.divide(number)){
-            return number5.getValue();
-        }
-        else if(number7.divide(number)){
-            return number7.getValue();
+        //sto supponendo che dato un input una e una sola regola viene applicata, e si può stabilire una gerarchia di regole
+
+        for(SostitutionRuleInterface rule : listOfRule){ //la gerarchia di regole si applica nell'ordine degli elementi inseriti nella lista
+            if(rule.divide(number)){
+                return rule.getValue();
+            }
         }
 
         return Integer.toString(number);
