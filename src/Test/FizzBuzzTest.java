f7c184dfda016cb4488438902b2d7bc8f1c8ce29
nicolalandro <nicolaxx94@live.it>
2016-12-21 12:53:23 +0100

    Refactor on FizzBuzzTest

--- src/Test/FizzBuzzTest.java
+++ src/Test/FizzBuzzTest.java
@@ -1,48 +1,32 @@
 package Test;
 
 import NoUtil.FizzBuzz;
+import junit.framework.TestCase;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
-/**
- * Created by nick on 19/12/16.
- */
-public class FizzBuzzTest {
 
-    @Test
-    public void creation(){
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
+public class FizzBuzzTest extends TestCase {
+    FizzBuzz fizzBuzz = new FizzBuzz();
+    public void test_creation()throws Exception{
         assertNotEquals(null, fizzBuzz);
     }
 
-    @Test
-    public void expected_1_FromInput1(){
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
+    public void test_expected_1_FromInput1()throws Exception{
         assertEquals("1", fizzBuzz.getValue(1));
     }
 
-    @Test
-    public void expected_fizz_FromInput3(){
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
+    public void test_expected_fizz_FromInput3()throws Exception{
         assertEquals("fizz", fizzBuzz.getValue(3));
     }
 
-    @Test
-    public void expected_fizz_FromInput5(){
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
+    public void test_expected_fizz_FromInput5()throws Exception{
         assertEquals("buzz", fizzBuzz.getValue(5));
     }
 
-    @Test
-    public void expected_bang_FromInput7(){
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
+    public void test_expected_bang_FromInput7()throws Exception{
         assertEquals("bang", fizzBuzz.getValue(7));
     }
 }
