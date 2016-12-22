13b4ece097b59fbbe3a6f61dfe6aba28db3d7cdf
nicolalandro <nicolaxx94@live.it>
2016-12-22 15:40:23 +0100

    Add test on Number 0

--- src/Test/FizzBuzzTest.java
+++ src/Test/FizzBuzzTest.java
@@ -37,27 +37,34 @@ public class FizzBuzzTest extends TestCase {
         assertEquals("1", fizzBuzz.getValue(1));
     }
 
+
+
+    public void test_expected_fizz_FromInput3()throws Exception{
+        assertEquals("fizz", fizzBuzz.getValue(3));
+    }
+
     public void test_expected_fizz_FromInput9()throws Exception{
         assertEquals("fizz", fizzBuzz.getValue(9));
+    }
+
+
+
+    public void test_expected_buzz_FromInput5()throws Exception{
+        assertEquals("buzz", fizzBuzz.getValue(5));
     }
 
     public void test_expected_buzz_FromInput25()throws Exception{
         assertEquals("buzz", fizzBuzz.getValue(25));
     }
 
-    public void test_expected_bang_FromInput49()throws Exception{
-        assertEquals("bang", fizzBuzz.getValue(49));
-    }
 
-    public void test_expected_fizz_FromInput3()throws Exception{
-        assertEquals("fizz", fizzBuzz.getValue(3));
-    }
-
-    public void test_expected_buzz_FromInput5()throws Exception{
-        assertEquals("buzz", fizzBuzz.getValue(5));
-    }
 
     public void test_expected_bang_FromInput7()throws Exception{
         assertEquals("bang", fizzBuzz.getValue(7));
     }
+
+    public void test_expected_bang_FromInput49()throws Exception{
+        assertEquals("bang", fizzBuzz.getValue(49));
+    }
+
 }
--- src/Test/RulesNumber0Test.java
+++ src/Test/RulesNumber0Test.java
@@ -9,6 +9,11 @@ public class RulesNumber0Test extends TestCase {
     public void test_RuleNumber0_isSatisfyFor_0() throws Exception {
         assertTrue(rule0.isSatisfyFor(0));
     }
+
+    public void test_RuleNumber0_not_isSatisfyFor_1() throws Exception {
+        assertFalse(rule0.isSatisfyFor(1));
+    }
+
     public void test_RuleNumber0_apply_expect_0(){
         assertEquals("0", rule0.apply());
     }
