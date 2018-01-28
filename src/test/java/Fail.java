7994754a855836262920a63f32b134e773114527
nicolalandro <nicolaxx94@live.it>
2018-01-28 09:47:53 +0100

    fail build

--- src/test/java/Fail.java
+++ src/test/java/Fail.java
@@ -1,13 +1,11 @@
 import org.junit.Test;
-import util.Counter;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 
 
 public class Fail {
     @Test
     public void creation() {
-        assertNotEquals(1, 0);
+        assertEquals(1, 0);
     }
 }
